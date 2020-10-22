11e987cc968f1f266027be3a527837b7205cd7f1
Alex Reed <alexreed1192@gmail.com>
2020-10-22 10:18:04 -0700

    add requirements.txt

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
+with open(file="README.md", mode="r") as fh:
     long_description = fh.read()
 
 setup(
@@ -13,15 +13,18 @@ setup(
     long_description_content_type="text/markdown",
     url='https://github.com/areed1192/portfolio-optimization',
     install_requires=[
-        'requests',
-        'pandas',
-        'fake-useragent',
-        'python-dateutil',
-        'scikit-learn',
-        'matplotlib',
-        'scipy'
+        'requests==2.24.0',
+        'matplotlib==3.3.0',
+        'numpy==1.19.0',
+        'pandas==1.0.5',
+        'scipy==1.5.2',
+        'fake_useragent==0.1.11',
+        'python_dateutil==2.8.1',
+        'scikit_learn==0.23.2'
     ],
-    packages=find_packages(include=['pyopt']),
+    packages=find_packages(
+        include=['pyopt']
+    ),
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Financial and Insurance Industry',
